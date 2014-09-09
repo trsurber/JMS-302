4cafb04d3ed6577276209ca754f7d170e1e0b28f
trsurber@belldouth.net <Tricia Surber>
2014-09-09 10:39:54 -0400

    in class exercises

---
+++ exercises/class_7/name2.rb
@@ -0,0 +1,9 @@
+puts "What is your first name?"
+name = gets.chomp
+#get the users name
+
+if name == 'Tricia' || name == 'tricia'
+    puts "What a lovely first name."
+else 
+    puts "Meh. Your name's alright."
+end
